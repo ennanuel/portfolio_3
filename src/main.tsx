2f6927e3a900bf131221cd93546b369288cdd48c
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-09 21:46:36 +0100

    Utilized ApolloProvider and apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+
+import App from './App.tsx';
+import client from './apolloClient.ts';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </StrictMode>,
 )
