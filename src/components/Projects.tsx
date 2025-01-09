55ba1df6d7b1eb98eb7c694a34cee98e2e0f642b
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-09 21:50:59 +0100

    Made minor refactoring changes

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,70 +9,18 @@ import { motion } from "framer-motion";
 import Button from "./Button";
 import Project from "./Project";
 import ProjectsDescription from "./ProjectsDescription";
-
-const PROJECTS = [
-    {
-        title: "Entuitive Engineering",
-        desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
-        image: "",
-        video: "",
-        demoLink: "",
-        category: "Architecture & Engineering",
-        tags: ["Design", "Development"],
-        date: "2024"
-    },
-    {
-        title: "Entuitive Engineering",
-        desc: "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum",
-        image: "",
-        video: "",
-        demoLink: "",
-        category: "Architecture & Engineering",
-        tags: ["Design", "Development"],
-        date: "2024"
-    },
-    {
-        title: "Entuitive Engineering",
-        desc: "Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        image: "",
-        video: "",
-        demoLink: "",
-        category: "Architecture & Engineering",
-        tags: ["Design", "Development"],
-        date: "2024"
-    },
-    {
-        title: "Entuitive Engineering",
-        desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words",
-        image: "",
-        video: "",
-        demoLink: "",
-        category: "Architecture & Engineering",
-        tags: ["Design", "Development"],
-        date: "2024"
-    },
-    {
-        title: "Entuitive Engineering",
-        desc: "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.",
-        image: "",
-        video: "",
-        demoLink: "",
-        category: "Architecture & Engineering",
-        tags: ["Design", "Development"],
-        date: "2024"
-    },
-];
+import { EzemaProjects } from "../types";
 
 const ease = [.16, 1, .3, 1];
 
-export default function Projects() {
+export default function Projects({ projects }: { projects: EzemaProjects[] }) {
     const containerRef = useRef<HTMLDivElement>(null);
     const buttonRef = useRef<HTMLDivElement>(null);
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
     const [index, setIndex] = useState(-1);
     const translateY = useMemo(() => `${index * -100}%`, [index]);
-    const projects = useMemo(() => PROJECTS.map((project, index) => ({ index: index + 1, description: project.desc })), []);
+    const myProjects = useMemo(() => projects.map(({ desc, code_url }, index) => ({ index: index + 1, desc, code_url })), [projects]);
 
     const [buttonPosition, setButtonPosition] = useState({ x: 0, y: 0 });
     const [hoveringOverProject, setHoveringOverProject] = useState(false);
@@ -88,6 +36,10 @@ export default function Projects() {
     const handleButtonMouseOut = () => setButtonPosition({ x: 0, y: 0 });
     const handleProjectMouseOver = () => setHoveringOverProject(true);
     const handleProjectMouseOut = () => setHoveringOverProject(false);
+
+    const visitLink = (href: string) => {
+        location.href = href;
+    }
     
     const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (event) => setMousePosition({ x: event.clientX, y: event.clientY });
 
@@ -100,7 +52,7 @@ export default function Projects() {
                         <span className="opacity-0 tracking-tighter">0</span>
                         <motion.div animate={{ y: translateY }} transition={{ ease, duration: 2 }} className="absolute h-full top-0 left-0 flex flex-col">
                             {
-                                projects.map(({ index }) => (
+                                myProjects.map(({ index }) => (
                                     <span className="tracking-tighter">{index}.</span>
                                 ))
                             }
@@ -109,7 +61,7 @@ export default function Projects() {
                     <span className="block">.</span>
                 </span>
                 <div className="flex flex-col gap-8">
-                    <ProjectsDescription projects={projects} currentIndex={index} />
+                    <ProjectsDescription projects={myProjects} currentIndex={index} />
                     <motion.div 
                         onMouseMove={handleButtonMouseMove} 
                         onMouseOut={handleButtonMouseOut} 
@@ -118,15 +70,21 @@ export default function Projects() {
                         className="overflow-hidden transition-transform ease-expo duration-500 w-fit"
                     >
                         <motion.div ref={buttonRef} transition={{ ease, duration: 2 }} initial={{ y: '100%' }} whileInView={{ y: 0, offset: 104 }} className="group w-max flex items-center gap-1 text-black-25">
-                            <Button text="View Site" Icon={MdOutlineArrowOutward} noGap className="font-mono vg-black-90 text-black-25 uppercase px-4 py-3 rounded-md"></Button>
+                            <Button 
+                                text="View Site"
+                                onClick={projects[index] ? () => visitLink(projects[index].demo_url) : undefined}
+                                Icon={MdOutlineArrowOutward} 
+                                noGap 
+                                className="font-mono vg-black-90 text-black-25 uppercase px-4 py-3 rounded-md"
+                            />
                         </motion.div>
                     </motion.div>
                 </div>
             </div>
             <ul className="peer flex-1 flex flex-col min-h-[200vh]">
                 {
-                    PROJECTS.map((project, index) => <Project 
-                        {...project} 
+                    projects.map((project, index) => <Project 
+                        project={project}
                         handleHover={handleProjectMouseOver} 
                         handleMouseOut={handleProjectMouseOut} 
                         index={index} 
@@ -149,14 +107,14 @@ export default function Projects() {
                     className={`transition-[transform,_opacity] ease-expo origin-top-left p-[1px] w-[128px] aspect-square absolute top-0 left-0 text-sm font-mono rounded-full bg-brown-900 border border-brown-700 pointer-events-none`}
                 >
                 </motion.div>
-                <motion.div 
+                <motion.span
                     animate={{ x: mousePosition.x, y: mousePosition.y, opacity: hoveringOverProject ? 1 : 0 }}
                     transition={{ ease, duration: .35 }}
                     className={`transition-[transform,_opacity] ease-expo origin-top-left p-[1px] w-[128px] flex flex-col items-center justify-center gap-2 aspect-square absolute top-0 left-0 text-sm font-mono text-brown-500 pointer-events-none`}
                 >
                     <span>Visit Site</span>
                     <MdOutlineArrowOutward size={20} />
-                </motion.div>  
+                </motion.span>  
             </div>
         </div>
     )
