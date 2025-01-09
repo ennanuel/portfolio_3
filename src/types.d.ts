e2d1a33dbc10cc95e625e0a0c4b775d2f0f581bc
Eunoia <ezemaemmanuel6@gmail.com>
2025-01-09 21:46:06 +0100

    Added file for type declaration exports

---
+++ src/types.d.ts
@@ -0,0 +1,39 @@
+
+
+export type EzemaProjects = {
+    title: string;
+    desc: string;
+    demo_url: string;
+    code_url: string;
+    category: string;
+    tags: string[];
+    image_public_id: string;
+    image_url: string;
+    video_public_id: string;
+    video_url: string;
+    createdAt: string;
+}
+    
+export type EzemaTestimonials = {
+    id: string;
+    verdict: string;
+    client_name: string;
+    client_role: string;
+    client_company: string;
+    services: string[];
+    client_image: {
+        publicUrl: string;
+    }
+};
+
+export type EzemaSocials = {
+    id: string;
+    platform: string;
+    url_link: string;
+};
+
+export type QueryResult = {
+    ezemaProjects: EzemaProjects[];
+    ezemaTestimonials: EzemaTestimonials[];
+    ezemaSocials: EzemaSocials[];
+}
