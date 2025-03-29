a547af3baf45592acac721326a2267d68849fc76
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-03-29 07:10:15 +0100

    Removed unused apolloClient file

--- src/apolloClient.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-
-const client = new ApolloClient({
-    uri: `${import.meta.env.VITE_BACKEND_URL}/api/graphql`,
-    cache: new InMemoryCache()
-})
-
-export default client;
