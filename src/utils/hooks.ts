4858071da8f580c7b45f26bb6b0f62195e4dfc57
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-03-29 06:45:38 +0100

    Added 'useFetch' hook for handling fetch

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,4 +1,12 @@
 import { useEffect, useState } from "react";
+import { QueryResult } from "../types";
+
+
+const THINGS_TO_FETCH = [
+    { name: "projects", path: "ezema/projects" },
+    { name: "testimonials", path: "ezema/testimonials" },
+    { name: "socials", path: "ezema/socials" }
+]
 
 
 export function useGetMousePosition() {
@@ -12,4 +20,34 @@ export function useGetMousePosition() {
     }, []);
 
     return mousePosition;
+};
+
+
+export function useFetch(options?: RequestInit) {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [data, setData] = useState<QueryResult>({});
+
+    useEffect(() => {
+        async function fetchData() {
+            setLoading(true);
+            for(const { path } of THINGS_TO_FETCH) {
+                try {
+                    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/${path}`, options);
+                    const result = await response.json();
+
+                    if(response.status !== 200) throw result;
+                    setData((prev) => ({ ...prev, ...result }));
+                } catch (error) {
+                    console.error((error as Error).message);
+                    setError(true);
+                }
+            }
+            setLoading(false);
+        }
+
+        fetchData();
+    }, []);
+
+    return { loading, error, data }
 }
