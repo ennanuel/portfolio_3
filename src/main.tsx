1c5b9848c8e759c7576302b96dfcab034f1a9f21
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-03-29 06:46:52 +0100

    Removed ApolloClient Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
 
 import App from './App.tsx';
-import client from './apolloClient.ts';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
