b9b96f81091521a9a05c7ac42946fa33ad845e68
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-06-13 22:21:36 +0100

    Added file for storing and exporting contacts

---
+++ src/assets/constants.ts
@@ -0,0 +1,28 @@
+
+
+export const MENUS = [
+    {
+        platform: "Home",
+        url_link: "#"
+    },
+    {
+        platform: "Services",
+        url_link: "#services"
+    },
+    {
+        platform: "Works",
+        url_link: "#projects"
+    },
+    {
+        platform: "About",
+        url_link: "#about"
+    },
+    {
+        platform: "Testimonials",
+        url_link: "#testimonials"
+    },
+    {
+        platform: "Contact",
+        url_link: "#contact"
+    }
+];
