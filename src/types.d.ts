53bfaf0e7837a4eb81ee366da4ac59fd77d4bc8c
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-06-13 22:17:11 +0100

    Exported new type declarations for component properties

--- src/types.d.ts
+++ src/types.d.ts
@@ -36,4 +36,58 @@ export type QueryResult = {
     projects?: EzemaProjects[];
     testimonials?: EzemaTestimonials[];
     socials?: EzemaSocials[];
-}
+};
+
+
+type PathType = string | null;
+
+export type HeaderProps = {
+    socials?: EzemaSocials[];
+    showingContentFor: PathType;
+};
+
+export type MenuProps = {
+    socials?: EzemaSocials[];
+    menuIsOpen: boolean;
+    closeModal: () => void;
+    showingContentFor: PathType;
+};
+
+export type IntroSectionProps = {
+    socials?: EzemaSocials[];
+    showingContentFor: PathType;
+};
+
+export type ServicesSectionProps = {
+    showingContentFor: PathType;
+};
+
+export type ServiceProps = { 
+    index: number; 
+    company?: string;
+    Icon?: IconType;
+    total: number; 
+    subTitle: string; 
+    title: string; 
+    description: string; 
+    subServices: string[]; 
+    topHeight: string;
+    titleFontSize: string;
+    titlePadding: string;
+    showingContentFor: PathType;
+};
+
+export type ProjectsSectionProps = {
+    projects?: EzemaProjects[];
+    showingContentFor: PathType;
+};
+
+export type TestimonialsSectionProps = {
+    testimonials?: EzemaTestimonials[];
+    showingContentFor: PathType;
+};
+
+export type FooterProps = {
+    socials?: EzemaSocials[];
+    showingContentFor: PathType;
+};
